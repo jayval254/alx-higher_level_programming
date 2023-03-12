3a5762f8f2fe832ac6b1600b0eacb9326e5fa716
jayval254 <valariekimuge1@gmail.com>
2023-03-12 10:37:58 -0700

    data structures

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    """
+    Prints a matrix of integers
+    Args:
+        matrix: a double dimension list
+    Returns:
+        Nothing, just print out the matrix
+    """
+    for item in matrix:
+        l = len(item)
+        if l == 0:
+            print()
+        for i in range(l):
+            print("{:d}".format(item[i]), end="\n" if i == l - 1 else " ")
