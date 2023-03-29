79fa48555a1cd3692ba7394a239d4a83d4cbc451
jayval254 <valariekimuge1@gmail.com>
2023-03-29 05:24:27 -0700

    python classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Square module"""
+
+class Square:
+    """a class Square that defines a square"""
+    def __init__(self, size_square):
+        """Module __init__ for size square"""
+        self.__size = size_square
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This module creates a square instance with a
+size and validates it is an int >= 0"""
+
+
+class Square:
+    """
+    Square Class that holds size
+    Attributes:
+        __size (integer): the size of the square instance
+    """
+    def __init__(self, new_size=0):
+        """
+        Initializes a square of size new_size
+        Args:
+            new_size: the size for the new square instance
+        """
+        if isinstance(new_size, int) is True:
+            if new_size >= 0:
+                self.__size = new_size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
