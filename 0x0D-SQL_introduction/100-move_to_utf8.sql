68c273e309c277e6739d001366ed84dbca065124
jayval254 <valariekimuge1@gmail.com>
2023-05-16 07:44:48 -0700

    SQL Introduction

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,7 +1,7 @@
 -- convert hbtn_0c_0 database to UTF8(utf8mb4, collate utf8mb4_unicode_ci) 
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- Alter the charset coding in the table first_table
-ALTER TABLE hbtn_0c_0.first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- Alter the charset coding in first_table.name
-ALTER TABLE hbtn_0c_0.first_table
+ALTER TABLE first_table
 MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
