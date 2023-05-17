21c980299a83b4790b3b0eab1d9ee1bb0e6f6f1e
jayval254 <valariekimuge1@gmail.com>
2023-05-17 11:45:43 -0700

    AirBnb Clone

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,4 @@
+-- lists all shows contained in hbtn_od_tvshows
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_show_genres INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id
