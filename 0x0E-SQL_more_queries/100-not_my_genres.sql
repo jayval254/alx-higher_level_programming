8de8fa7a257823853fae2cdcf572e47d5e7ecea6
jayval254 <valariekimuge1@gmail.com>
2023-05-26 06:13:50 -0700

    SQL more queries

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,16 +1,16 @@
 -- uses hbtn_od_tvshows database to list all genres linked to the show Dexter
-SELECT DISTINCT     genre.name
-FROM                tv_genres               AS genre
-    INNER JOIN      tv_show_genres          AS showgenre
+SELECT DISTINCT genre.name
+FROM tv_genres AS genre
+    INNER JOIN tv_show_genres AS showgenre
         ON genre.id = showgenre.genre_id
-        INNER JOIN tv_shows                 AS show
+        INNER JOIN tv_shows AS show
             ON showgenre.show_id = show.id
 WHERE genre.name NOT IN (
-    SELECT          genre.name 
-    FROM            tv_genres               AS genre
-        INNER JOIN tv_show_genres           AS showgenre
+    SELECT genre.name 
+    FROM tv_genres AS genre
+        INNER JOIN tv_show_genres AS showgenre
             ON genre.id = showgenre.genre_id
-            INNER JOIN tv_shows             AS show
+            INNER JOIN tv_shows AS show
                 ON showgenre.show_id = show.id
     WHERE show.title = "Dexter"
 )
