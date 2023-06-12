66298479dd5ec896aee7bb447c64d5a0a2c86b53
jayval254 <valariekimuge1@gmail.com>
2023-06-12 14:32:28 -0700

    javascript warm up

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (process.argv.length === 1) {
+  console.log('Argument found');
+} else if (process.argv.length > 1) {
+  console.log('Arguments found');
+} else {
+  console.log('No argument');
+}
