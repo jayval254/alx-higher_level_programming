574ca7e490c1489e2bfeacf3bcb1da9d212dfc9f
jayval254 <valariekimuge1@gmail.com>
2023-06-13 08:28:32 -0700

    javascript objects scopes closure

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let occurencesCounter = 0;
+  for (const item of list) {
+    if (item === searchElement) {
+      occurencesCounter++;
+    }
+  }
+  return occurencesCounter;
+};
