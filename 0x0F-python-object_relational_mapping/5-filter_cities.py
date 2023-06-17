dfd82fac7f5022acb63b2dbe5cc356ca64d15ad1
jayval254 <valariekimuge1@gmail.com>
2023-06-17 11:30:42 -0700

    pythom ORM

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,35 +1,37 @@
 #!/usr/bin/python3
+""" lists all cities of a state
 """
-SelectStates module
-"""
-import MySQLdb
-import sys
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
 
-
-def list_cities():
-    """lists all cities of a state in database"""
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    USERNAME = sys.argv[1]
+    PASSWORD = sys.argv[2]
+    DATABASE = sys.argv[3]
     searched = sys.argv[4]
 
-    db = MySQLdb.connect(host='localhost',
-                         port=3306,
-                         user=username,
-                         passwd=password,
-                         db=database
-                         )
+    db = MySQLdb.connect(
+        host="localhost",
+        user=USERNAME,
+        passwd=PASSWORD,
+        db=DATABASE,
+        port=3306)
+
     cur = db.cursor()
-    cur.execute("SELECT cities.name FROM cities INNER JOIN states ON cities.state_id = states.id WHERE states.name = %(state_name)s ORDER BY cities.id ASC", {'state_name': searched})
+
+    cur.execute("SELECT cities.name, states.name \
+                FROM cities INNER JOIN states on cities.state_id = states.id\
+                ORDER BY cities.id ASC")
     rows = cur.fetchall()
-    lst = []
+
+    list = []
+
     for row in rows:
-        for col in row:
-            lst.append(col)
-    print(', '.join(lst))
+        if row[1] == searched:
+            list.append(row[0])
+
+    output = ", ".join(list)
+    print(output)
+
     cur.close()
     db.close()
-
-if __name__ == "__main__":
-    list_cities()
