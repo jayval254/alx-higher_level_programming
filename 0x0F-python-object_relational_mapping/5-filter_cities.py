dc62238dedb4a42a3658e7b6f6d3e716787d1636
jayval254 <valariekimuge1@gmail.com>
2023-06-17 08:16:34 -0700

    pythom ORM

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+SelectStates module
+"""
+import MySQLdb
+import sys
+
+
+def list_cities():
+    """lists all cities of a state in database"""
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    searched = sys.argv[4]
+
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=database
+                         )
+    cur = db.cursor()
+    cur.execute("SELECT cities.name FROM cities INNER JOIN states ON cities.state_id = states.id WHERE states.name = %(state_name)s ORDER BY cities.id ASC", {'state_name': searched})
+    rows = cur.fetchall()
+    lst = []
+    for row in rows:
+        for col in row:
+            lst.append(col)
+    print(', '.join(lst))
+    cur.close()
+    db.close()
+
+if __name__ == "__main__":
+    list_cities()
