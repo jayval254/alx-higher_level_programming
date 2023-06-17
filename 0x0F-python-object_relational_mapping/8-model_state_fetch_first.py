1b30897b245c56b0a8f324430a11a92431054cdf
jayval254 <valariekimuge1@gmail.com>
2023-06-17 11:36:14 -0700

    pythom ORM

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,33 +1,21 @@
 #!/usr/bin/python3
 """
-State module
+prints the first State object from the database
 """
-from sqlalchemy import text
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
-from model_state import Base, State
-import sys
+if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import Session
 
-
-def fetch_first():
-    """Lists all state objects"""
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        username, password, database), pool_pre_ping=True)
-
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    Session = sessionmaker()
-    Session.configure(bind=engine)
-    session = Session()
-    state = session.query(State).first()
-    if state:
+
+    session = Session(engine)
+    state = session.query(State).order_by(State.id).first()
+    if (state):
         print("{}: {}".format(state.id, state.name))
     else:
         print("Nothing")
     session.close()
-
-if __name__ == "__main__":
-    fetch_first()
